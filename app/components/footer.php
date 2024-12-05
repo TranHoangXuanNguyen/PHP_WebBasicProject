91e4332eb4214b6fd971ce60fe75a7e2305ebc46
HoThiDuyenHa <hothiduyenhai2005@gmail.com>
2024-12-06 06:56:28 +0700

    #2-haupdate_footer

--- app/components/footer.php
+++ app/components/footer.php
@@ -1,1 +1,91 @@
 <!-- code base -->
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Footer Example</title>
+  <!-- Thêm liên kết đến Bootstrap CSS -->
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="../assets/css/footer.css">
+</head>
+<body>
+  <!-- Footer -->
+  <footer class="footer">
+        <div class="container">
+                <div class="row ">
+                        <div class="col-md-6 text-left">
+                              <div class="title">
+                                    <h6><b>Still You Need Our Support ?</b></h6>
+                                    <p>Don't wait make a smart & logical quote here. Its pretty easy.</p>
+                              </div>
+                        </div>
+                        <div class="col-md-6">
+                                <div class="subscribe-form">
+                                    <input type="email" placeholder="Enter Your Email" class="email-input">
+                                    <button>Subscribe Now</button>
+                                </div>
+                        </div>
+                </div>    
+                <div class="row">
+                        <div class="col-md-4">
+                               <h6><b>About Us</b></h6>
+                               <p>Mama's kitchen, best choice for you</p>
+                               <div class="time">
+                                    <div class="clock-icon">
+                                                <i class="far fa-clock" style="font-size:40px"></i>
+                                                <!-- <i class="far fa-clock"></i> -->
+                                    </div>
+                                    <div class="time-text">
+                                              <p><strong>Opening Hours:</strong></p>
+                                              <p>Mon - Sat(8:00-6:00)</p>
+                                              <p>Sunday - Closed</p>
+                                    </div>
+                               </div>  
+                        </div>
+                        <div class="col-md-4">
+                               <h6><b>UsefulLinks</b></h6>
+                               <ul>
+                                   <li><a href="#">About</a></li>
+                                   <li><a href="#">News</a></li>
+                                   <li><a href="#">Partners</a></li>
+                                   <li><a href="#">Team</a></li>
+                                   <li><a href="#">Menu</a></li>
+                                   <li><a href="#">Contacts</a></li>
+                               </ul>
+                        </div>
+                        <div class="col-md-4">
+                               <h6><b>Help</b></h6>
+                               <ul>
+                                   <li><a href="#">FAQ</a></li>
+                                   <li><a href="#">Term & conditions</a></li>
+                                   <li><a href="#">Reporting</a></li>
+                                   <li><a href="#">Documentation</a></li>
+                                   <li><a href="#">Support Policy</a></li>
+                                   <li><a href="#">Privacy</a></li>
+                               </ul>
+                        </div>
+                        
+                </div>
+
+                <div class="row">
+                        <div class="col-md-6 text-under">
+                                <p>Myteam © 2024 by Mama's Kitchen All Rights Reserved.</p>
+                        </div>
+                        <div class="col-md-6 icon-right">
+                                <i class="fab fa-facebook-square" style="font-size:36px;"></i>
+                                <i class="fab fa-twitter-square" style="font-size:36px;"></i>
+                                <i class="fab fa-instagram-square" style="font-size:36px;"></i>
+                                <i class="fab fa-youtube-square" style="font-size:36px;"></i>
+                                <i class="fab fa-pinterest-square" style="font-size:36px;"></i>
+                        </div>
+                </div>
+              
+        </div>
+  
+  </footer>
+
+
+</body>
+</html>
