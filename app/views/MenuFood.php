3e200b37a243777384eb2a5fe02cfffa16348ce0
Xabe04-14 <be.y26@student.passerellesnumeriques.org>
2024-12-15 18:44:09 +0700

    menuFood_UI

---
+++ app/views/MenuFood.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MenuFood</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
+    <?php
+    require_once("./app/assets/css/menuFood.css.php");
+    ?>
+</head>
+
+<body>
+    <?php
+    require_once("app/components/header.php");
+    ?>
+    <div class="menufood-container">
+        <!-- Row 1 -->
+        <div class="row mb-5 ">
+            <div class="col-sm-4 image-container">
+                <img src="https://images.immediate.co.uk/production/volatile/sites/2/2024/05/GurdeepBacalhausaltCodFrittersSaffron-076-f07f1be.gif?quality=90&webp=true&resize=375,341" alt="Check Icon">
+            </div>
+            <div class="col-sm-6">
+                <div class="category">STARTER MENU</div>
+                <div class="nameFood">
+                    <div class="row-content d-flex justify-content-between">
+                        <div class="item">Fish Cake Rolled In Green Rice</div>
+                        <div class="price">$10</div>
+                    </div>
+                    <div class="row-content d-flex justify-content-between align-items-center">
+                        <div class="item">Dumplings</div>
+                        <div class="price">$10</div>
+                    </div>
+                    <div class="row-content d-flex justify-content-between align-items-center">
+                        <div class="item">Grilled Spring Rolls</div>
+                        <div class="price">$10</div>
+                    </div>
+                    <div class="row-content d-flex justify-content-between align-items-center">
+                        <div class="item">Vegetable Salad</div>
+                        <div class="price">$10</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Row 2 -->
+        <div class="row mb-5 ">
+            <div class="col-sm-4 image-container">
+                <img src="https://images.immediate.co.uk/production/volatile/sites/2/2024/05/GurdeepBacalhausaltCodFrittersSaffron-076-f07f1be.gif?quality=90&webp=true&resize=375,341" alt="Check Icon">
+            </div>
+            <div class="col-sm-6">
+                <div class="category">STARTER MENU</div>
+                <div class="nameFood">
+                    <div class="row-content d-flex justify-content-between">
+                        <div class="item">Fish Cake Rolled In Green Rice</div>
+                        <div class="price">$10</div>
+                    </div>
+                    <div class="row-content d-flex justify-content-between align-items-center">
+                        <div class="item">Dumplings</div>
+                        <div class="price">$10</div>
+                    </div>
+                    <div class="row-content d-flex justify-content-between align-items-center">
+                        <div class="item">Grilled Spring Rolls</div>
+                        <div class="price">$10</div>
+                    </div>
+                    <div class="row-content d-flex justify-content-between align-items-center">
+                        <div class="item">Vegetable Salad</div>
+                        <div class="price">$10</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Row 3 -->
+        <div class="row mb-5 ">
+            <div class="col-sm-4 image-container">
+                <img src="https://images.immediate.co.uk/production/volatile/sites/2/2024/05/GurdeepBacalhausaltCodFrittersSaffron-076-f07f1be.gif?quality=90&webp=true&resize=375,341" alt="Check Icon">
+            </div>
+            <div class="col-sm-6">
+                <div class="category">STARTER MENU</div>
+                <div class="nameFood">
+                    <div class="row-content d-flex justify-content-between">
+                        <div class="item">Fish Cake Rolled In Green Rice</div>
+                        <div class="price">$10</div>
+                    </div>
+                    <div class="row-content d-flex justify-content-between align-items-center">
+                        <div class="item">Dumplings</div>
+                        <div class="price">$10</div>
+                    </div>
+                    <div class="row-content d-flex justify-content-between align-items-center">
+                        <div class="item">Grilled Spring Rolls</div>
+                        <div class="price">$10</div>
+                    </div>
+                    <div class="row-content d-flex justify-content-between align-items-center">
+                        <div class="item">Vegetable Salad</div>
+                        <div class="price">$10</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Row 4 -->
+        <div class="row mb-5 ">
+            <div class="col-sm-4 image-container">
+                <img src="https://images.immediate.co.uk/production/volatile/sites/2/2024/05/GurdeepBacalhausaltCodFrittersSaffron-076-f07f1be.gif?quality=90&webp=true&resize=375,341" alt="Check Icon">
+            </div>
+            <div class="col-sm-6">
+                <div class="category">STARTER MENU</div>
+                <div class="nameFood">
+                    <div class="row-content d-flex justify-content-between">
+                        <div class="item">Fish Cake Rolled In Green Rice</div>
+                        <div class="price">$10</div>
+                    </div>
+                    <div class="row-content d-flex justify-content-between align-items-center">
+                        <div class="item">Dumplings</div>
+                        <div class="price">$10</div>
+                    </div>
+                    <div class="row-content d-flex justify-content-between align-items-center">
+                        <div class="item">Grilled Spring Rolls</div>
+                        <div class="price">$10</div>
+                    </div>
+                    <div class="row-content d-flex justify-content-between align-items-center">
+                        <div class="item">Vegetable Salad</div>
+                        <div class="price">$10</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php
+  include_once("app/components/footer.php");
+  ?>
+</body>
+</html>
