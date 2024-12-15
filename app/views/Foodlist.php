4b2be658a9da63e213323e2c50c31127659ade61
HoThiDuyenHa <hothiduyenhai2005@gmail.com>
2024-12-15 14:30:54 +0700

    #16-feature/doneFoodlist_UI

---
+++ app/views/Foodlist.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mamakitchen</title>
+  <?php
+  require_once("./app/assets/css/foodlist.css.php");
+  ?>
+</head>
+<body>
+  <?php
+  require_once("app/components/header.php");
+  ?>
+ <div class="container py-5 menu-list">
+            <!--  Ô tìm kiếm-->
+             <div class="row mb-4 search">
+                    <div class="input-wrapper">
+                        <input type="text" class="search-pd"placeholder="Search Product" name="search" >
+                        <i class="fa fa-search research"></i> <!-- Icon hình chìa khóa -->
+                 </div>
+             </div>
+             <!--  -->
+             <div class="row food-list">
+                        <!-- Food Item 1 -->
+                        <div class="col-md-4 mb-4 ">
+                                <div class="card h-100 food-item">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA6ZyFcSW30W1KDFKgxlx6umg9H1qe_EIqcQ&s" class="card-img-top" alt="Beef Jerky">
+                                    <div class="card-body food">
+                                        <p class="food-title">Beef Jerky</p>
+                                        <p class="price">30.000</p>
+                                    </div>
+                                </div>
+                            </div>
+                        <!-- Food Item 2 -->
+                        <div class="col-md-4 mb-4">
+                                <div class="card h-100 food-item">
+                                <img src="https://images.immediate.co.uk/production/volatile/sites/2/2024/05/GurdeepBacalhausaltCodFrittersSaffron-076-f07f1be.gif?quality=90&webp=true&resize=375,341" class="card-img-top" alt="Bread Pan">
+                                <div class="card-body food">
+                                        <p class="food-title">Bread Pan</p>
+                                        <p class="price">21.000</p>
+                                    </div>
+                                </div>
+                            </div>
+                        <!-- Food Item 3 -->
+                        <div class="col-md-4 mb-4">
+                                <div class="card h-100 food-item">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA6ZyFcSW30W1KDFKgxlx6umg9H1qe_EIqcQ&s" class="card-img-top" alt="Bread Pan">
+                                    <div class="card-body food">
+                                        <p class="food-title">Bread Pan</p>
+                                        <p class="price">21.000</p>
+                                    </div>
+                                </div>
+                        </div>
+                        <!-- Food Item 4 -->
+                        <div class="col-md-4 mb-4">
+                                <div class="card h-100 food-item">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA6ZyFcSW30W1KDFKgxlx6umg9H1qe_EIqcQ&s" class="card-img-top" alt="Bread Pan">
+                                    <div class="card-body food">
+                                        <p class="food-title">Bread Pan</p>
+                                        <p class="price">21.000</p>
+                                    </div>
+                                </div>
+                        </div>
+                 
+                             <!-- Food Item 5 -->
+                             <div class="col-md-4 mb-4">
+                                    <div class="card h-100 food-item">
+                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA6ZyFcSW30W1KDFKgxlx6umg9H1qe_EIqcQ&s" class="card-img-top" alt="Bread Pan">
+                                        <div class="card-body food">
+                                            <p class="food-title">Bread Pan</p>
+                                            <p class="price">21.000</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            <!-- Food Item 6-->
+                            <div class="col-md-4 mb-4">
+                                    <div class="card h-100 food-item">
+                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA6ZyFcSW30W1KDFKgxlx6umg9H1qe_EIqcQ&s" class="card-img-top" alt="Bread Pan">
+                                        <div class="card-body food">
+                                            <p class="food-title">Bread Pan</p>
+                                            <p class="price">21.000</p>
+                                        </div>
+                                    </div>
+                            </div>
+                            <!-- Food Item 7-->
+                            <div class="col-md-4 mb-4">
+                                    <div class="card h-100 food-item">
+                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA6ZyFcSW30W1KDFKgxlx6umg9H1qe_EIqcQ&s" class="card-img-top" alt="Bread Pan">
+                                        <div class="card-body food">
+                                            <p class="food-title">Bread Pan</p>
+                                            <p class="price">21.000</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            <!-- Food Item 8-->
+                            <div class="col-md-4 mb-4">
+                                    <div class="card h-100 food-item">
+                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA6ZyFcSW30W1KDFKgxlx6umg9H1qe_EIqcQ&s" class="card-img-top" alt="Bread Pan">
+                                        <div class="card-body food">
+                                            <p class="food-title">Bread Pan</p>
+                                            <p class="price">21.000</p>
+                                        </div>
+                                    </div>
+                                </div>
+             
+                            <!-- Food Item 9-->
+                            <div class="col-md-4 mb-4">
+                                    <div class="card h-100 food-item">
+                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA6ZyFcSW30W1KDFKgxlx6umg9H1qe_EIqcQ&s" class="card-img-top" alt="Bread Pan">
+                                        <div class="card-body food">
+                                            <p class="food-title">Bread Pan</p>
+                                            <p class="price">21.000</p>
+                                        </div>
+                                    </div>
+                            </div>
+                            <!-- Food Item 10-->
+                            <div class="col-md-4 mb-4">
+                                    <div class="card h-100 food-item">
+                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA6ZyFcSW30W1KDFKgxlx6umg9H1qe_EIqcQ&s" class="card-img-top" alt="Bread Pan">
+                                        <div class="card-body food">
+                                            <p class="food-title">Bread Pan</p>
+                                            <p class="price">21.000</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            <!-- Food Item 11-->
+                            <div class="col-md-4 mb-4">
+                                    <div class="card h-100 food-item">
+                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA6ZyFcSW30W1KDFKgxlx6umg9H1qe_EIqcQ&s" class="card-img-top" alt="Bread Pan">
+                                        <div class="card-body food">
+                                            <p class="food-title">Bread Pan</p>
+                                            <p class="price">21.000</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            <!-- Food Item 12-->
+                            <div class="col-md-4 mb-4">
+                                    <div class="card h-100 food-item">
+                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA6ZyFcSW30W1KDFKgxlx6umg9H1qe_EIqcQ&s" class="card-img-top" alt="Bread Pan">
+                                        <div class="card-body food">
+                                            <p class="food-title">Bread Pan</p>
+                                            <p class="price">21.000</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            
+            </div>
+
+        </div>
+
+<?php
+  include_once("app/components/footer.php");
+?>
+</body>
+
+</html>
