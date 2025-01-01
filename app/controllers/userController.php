e4aa77482daa1295812525f13b674fdda583e840
Tran Hoang Xuan Nguyen <hoangnguyendepgiai@gmail.com>
2025-01-01 21:21:20 +0700

    modified:   app/controllers/userController.php 	modified:   app/controllers/userController.php 	modified:   app/views/FoodList.php

--- app/controllers/userController.php
+++ app/controllers/userController.php
@@ -420,6 +420,8 @@ class userController extends Controller
             function execPostRequest($url, $data)
             {
                 $ch = curl_init(url: $url);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
                 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
                 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -437,6 +439,11 @@ class userController extends Controller
                 $result = curl_exec($ch);
 
                 curl_close($ch);
+                // if ($result === false) {
+                //     $error = curl_error($ch);
+                    // Xử lý lỗi
+                // }
+
                 return $result;
             }
 
